0fc4b01082745bbf138ea876768d814d02ae9e60
Domenico Manna <mannadomenico2849@gmail.com>
2019-09-11 21:56:29 -0400

    Updates

---
+++ precache-manifest.b60d1b659313cebdad6adcda63ed949e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db65ef84503c42fb34ec8560bb43f199",
+    "url": "/propertyManagementVersion2/index.html"
+  },
+  {
+    "revision": "02f37082db66332ae4ed",
+    "url": "/propertyManagementVersion2/static/css/main.6cfc032b.chunk.css"
+  },
+  {
+    "revision": "ba967bdde102af0885ee",
+    "url": "/propertyManagementVersion2/static/js/2.c4ad77c8.chunk.js"
+  },
+  {
+    "revision": "02f37082db66332ae4ed",
+    "url": "/propertyManagementVersion2/static/js/main.1d218f00.chunk.js"
+  },
+  {
+    "revision": "821b9469bfd7196f70d0",
+    "url": "/propertyManagementVersion2/static/js/runtime~main.adb6a117.js"
+  },
+  {
+    "revision": "21da357b373395b6095fc2e29faa322f",
+    "url": "/propertyManagementVersion2/static/media/homes.21da357b.jpg"
+  }
+]);
